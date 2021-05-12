43a7e082a3285399309fc94e51cb35a4ef89373b
ayushtiwari163 <ayushtiwari163@gmail.com>
2021-05-12 07:41:17 +0530

    Adding the left file

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = (props) => {
+    return (
+        
+        <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
+            <img alt="robots" src={`https://robohash.org/${props.id}?size=200x200`} />
+            <div>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
+            </div>
+        </div>
+        
+    )
+}
+export default Card
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import App from './container/App';
+ReactDOM.render(
+ <App />
+ ,document.getElementById('root')
+);
+
+
+reportWebVitals();
